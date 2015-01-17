f21bbc0787732dfc950224f563758c15fb47cca0
Charlie Rode <chrode10@gmail.com>
2015-01-17 15:08:28 -0800

    Add Profile model

--- dj_dashboard/dash_app/models.py
+++ dj_dashboard/dash_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User)
+    # Profile picture will be stored on AWS's S3
+    profile_pic = models.FileField(null=True, blank=True, max_length=150)
+    # May need the url 
+    url = models.CharField(null=True, max_length=150)
+    profile_blurb = models.CharField(null=True, max_length=1000)
